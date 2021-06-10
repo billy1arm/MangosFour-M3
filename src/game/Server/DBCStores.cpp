3399ca4cd7792349beebc91dcce70502df312d1c
Antz <antz@cix.co.uk>
2021-06-10 23:57:43 +0100

    Patch up to get server to load

--- src/game/Server/DBCStores.cpp
+++ src/game/Server/DBCStores.cpp
@@ -1050,9 +1050,9 @@ int lastGemPropertiesStore = 2250; // last gem property added in 4.3.4
 int lastMapStore = 980; // last map added in 4.3.4
 int lastSpellStore = 121820; // last added spell in 4.3.4
 #elif defined(MISTS)
-int lastAreaStore = 5401; // last area (areaflag) added in 5.4.8 (18414)
-int lastCharTitlesStore = 351; // last char title added in 5.4.8 (18414)
-int lastGemPropertiesStore = 2450; // last gem property added in 5.4.8 (18414)
+int lastAreaStore = 6863; // last area (areaflag) added in 5.4.8 (18414)
+int lastCharTitlesStore = 389; // last char title added in 5.4.8 (18414)
+int lastGemPropertiesStore = 2467; // last gem property added in 5.4.8 (18414)
 int lastMapStore = 1173; // last map added in 5.4.8 (18414)
 int lastSpellStore = 163227; // last added spell in 5.4.8 (18414)
 #endif
