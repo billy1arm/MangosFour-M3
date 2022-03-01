135cc45f66c63cd015fcda24d7de7693f26ec906
Antz <antz@cix.co.uk>
2022-03-01 01:38:00 +0000

    Enable Core Build Selection. Thanks Meltie

--- cmake/MangosParams.cmake
+++ cmake/MangosParams.cmake
@@ -1,7 +1,30 @@
-#set(MANGOS_EXP "CATA")
-#set(MANGOS_PKG "Mangos Three")
-set(MANGOS_EXP "MISTS")
-set(MANGOS_PKG "Mangos Four")
+if ("${SERVER_BUILD_TYPE}" STREQUAL "Classic")
+    set(MANGOS_EXP "CLASSIC")
+    set(MANGOS_PKG "Mangos Zero")
+endif ()
+
+if ("${SERVER_BUILD_TYPE}" STREQUAL "TBC")
+    set(MANGOS_EXP "TBC")
+    set(MANGOS_PKG "Mangos One")
+
+endif ()
+
+if ("${SERVER_BUILD_TYPE}" STREQUAL "WotLK")
+    set(MANGOS_EXP "WOTLK")
+    set(MANGOS_PKG "Mangos Two")
+endif ()
+
+if ("${SERVER_BUILD_TYPE}" STREQUAL "Cata")
+    set(MANGOS_EXP "CATA")
+    set(MANGOS_PKG "Mangos Three")
+endif ()
+
+if ("${SERVER_BUILD_TYPE}" STREQUAL "Mop")
+    set(MANGOS_EXP "MISTS")
+    set(MANGOS_PKG "Mangos Four")
+endif ()
+
+
 set(MANGOS_WORLD_VER 2021010100)
 set(MANGOS_REALM_VER 2021010100)
 set(MANGOS_AHBOT_VER 2021010100)
