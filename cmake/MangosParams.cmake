02aeeb9a181d4ff6154bbdb17f0337e5f4df2bec
Antz <antz@cix.co.uk>
2022-03-01 06:37:09 +0000

    Fix cmake file

--- cmake/MangosParams.cmake
+++ cmake/MangosParams.cmake
@@ -1,29 +1,28 @@
-if ("${SERVER_BUILD_TYPE}" STREQUAL "Classic")
+if ("${CLIENT_BUILD_TYPE}" STREQUAL "Classic")
     set(MANGOS_EXP "CLASSIC")
     set(MANGOS_PKG "Mangos Zero")
 endif ()
 
-if ("${SERVER_BUILD_TYPE}" STREQUAL "TBC")
+if ("${CLIENT_BUILD_TYPE}" STREQUAL "TBC")
     set(MANGOS_EXP "TBC")
     set(MANGOS_PKG "Mangos One")
 
 endif ()
 
-if ("${SERVER_BUILD_TYPE}" STREQUAL "WotLK")
+if ("${CLIENT_BUILD_TYPE}" STREQUAL "WotLK")
     set(MANGOS_EXP "WOTLK")
     set(MANGOS_PKG "Mangos Two")
 endif ()
 
-if ("${SERVER_BUILD_TYPE}" STREQUAL "Cata")
+if ("${CLIENT_BUILD_TYPE}" STREQUAL "Cata")
     set(MANGOS_EXP "CATA")
     set(MANGOS_PKG "Mangos Three")
 endif ()
 
-if ("${SERVER_BUILD_TYPE}" STREQUAL "Mop")
+if ("${CLIENT_BUILD_TYPE}" STREQUAL "Mop")
     set(MANGOS_EXP "MISTS")
     set(MANGOS_PKG "Mangos Four")
 endif ()
-
 
 set(MANGOS_WORLD_VER 2021010100)
 set(MANGOS_REALM_VER 2021010100)
